96c68608ceb3382e20363ee5bd993cd7399148c5
VINAY REDDY <vinaykumarreddy298@gmail.com>
2018-03-17 14:10:53 +0530

    This is first form
    change -Id:I454564545454
    Signed-off-by:vinay<vinay80801@gmail.com>

---
+++ src/Gtttt/Gittt.java
@@ -0,0 +1,17 @@
+package Gtttt;
+
+public class Gittt {
+
+	public void String(){
+		
+	}
+	
+public void String2(){
+		System.out.println("sdgdfg");
+		System.out.println("sdgdfg");
+	}
+	
+public static void main(String[] args) {
+	
+}
+}
